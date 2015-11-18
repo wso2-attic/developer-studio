b116b191732523dd87813fbd3d66d034fb92a046
knadikari <knadikari@gmail.com>
2015-11-18 09:49:38 +0530

    Bug fixed

--- test/org.wso2.developerstudio.eclipse.wso2plugin.sample.test/src/org/wso2/developerstudio/eclipse/wso2plugin/sample/test/TestWso2Plugin.java
+++ test/org.wso2.developerstudio.eclipse.wso2plugin.sample.test/src/org/wso2/developerstudio/eclipse/wso2plugin/sample/test/TestWso2Plugin.java
@@ -17,19 +17,21 @@
 package org.wso2.developerstudio.eclipse.swttests.testcase;
 
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.junit.Order;
+import org.junit.OrderedRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.AbstractClass;
 import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.Setup;
 
-@RunWith(SWTBotJunit4ClassRunner.class)
+@RunWith(OrderedRunner.class)
 public class TestWso2Plugin extends Setup {
 
-	@Test
-	public void test() {
+        @Test
+        @Order(order = 1)
+	public void createPluginPorject() {
 		AbstractClass.openProjectFromMenu("wso2");
-		bot.sleep(3000);
-		AbstractClass.createWso2Plugin("FirstPlugin");
+		AbstractClass.createWso2Plugin("FirstPlugin", "Single Module Project");
 	}
 
 }
