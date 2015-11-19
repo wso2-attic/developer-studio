c4abc5e3b1dc3342684b5a8caa060ece9fc6e857
knadikari <knadikari@gmail.com>
2015-11-19 17:11:45 +0530

    commit after rebase

--- test/org.wso2.developerstudio.eclipse.wso2plugin.sample.test/src/org/wso2/developerstudio/eclipse/wso2plugin/sample/test/TestWso2Plugin.java
+++ test/org.wso2.developerstudio.eclipse.wso2plugin.sample.test/src/org/wso2/developerstudio/eclipse/wso2plugin/sample/test/TestWso2Plugin.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.wso2.developerstudio.eclipse.swttests.testcase;
+package org.wso2.developerstudio.eclipse.wso2plugin.sample.test;
 
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.junit.Order;
@@ -23,15 +23,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.AbstractClass;
 import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.Setup;
+import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.Wso2PluginUtils;
 
-@RunWith(OrderedRunner.class)
+@RunWith(SWTBotJunit4ClassRunner.class)
 public class TestWso2Plugin extends Setup {
 
         @Test
         @Order(order = 1)
 	public void createPluginPorject() {
 		AbstractClass.openProjectFromMenu("wso2");
-		AbstractClass.createWso2Plugin("FirstPlugin", "Single Module Project");
+		Wso2PluginUtils.createWso2Plugin("FirstPlugin", "Single Module Project");
 	}
 
 }
