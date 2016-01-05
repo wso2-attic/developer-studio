16309d4af60e5cc6c787ce771bba54f091e9985c
Nuwan Chamara Pallewela <nwnpallewela@gmail.com>
2016-01-05 14:17:50 +0530

    Refactoring and Removing Assert operations from BAMMediatorTransformer and OAuthMediatorTransformer

--- esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/OAuthMediatorTransformer.java
+++ esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/OAuthMediatorTransformer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 WSO2, Inc. (http://wso2.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.developerstudio.eclipse.gmf.esb.internal.persistence;
 
 import java.util.List;
@@ -5,71 +21,68 @@ import java.util.List;
 import org.apache.commons.lang.StringUtils;
 import org.apache.synapse.endpoints.Endpoint;
 import org.apache.synapse.mediators.base.SequenceMediator;
-import org.eclipse.core.runtime.Assert;
 import org.eclipse.emf.ecore.EObject;
 import org.wso2.developerstudio.eclipse.gmf.esb.EsbNode;
 import org.wso2.developerstudio.eclipse.gmf.esb.OAuthMediator;
 import org.wso2.developerstudio.eclipse.gmf.esb.persistence.TransformationInfo;
 import org.wso2.developerstudio.eclipse.gmf.esb.persistence.TransformerException;
 
-public class OAuthMediatorTransformer extends AbstractEsbNodeTransformer  {
+public class OAuthMediatorTransformer extends AbstractEsbNodeTransformer {
 
-	public void transform(TransformationInfo information, EsbNode subject)
-			throws TransformerException {
-		// TODO Auto-generated method stub
-		information.getParentSequence().addChild(createOAuthMediator(subject));
-		// Transform the OAuth mediator output data flow path.
-		doTransform(information,
-				((OAuthMediator) subject).getOutputConnector());
-		
-	}
+    public void transform(TransformationInfo information, EsbNode subject) throws TransformerException {
+        information.getParentSequence().addChild(createOAuthMediator(subject));
+        // Transform the OAuth mediator output data flow path.
+        doTransform(information, ((OAuthMediator) subject).getOutputConnector());
 
-	public void createSynapseObject(TransformationInfo info, EObject subject,
-			List<Endpoint> endPoints) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	public void transformWithinSequence(TransformationInfo information,
-			EsbNode subject, SequenceMediator sequence) throws TransformerException {
-		// TODO Auto-generated method stub
-		sequence.addChild(createOAuthMediator(subject));
-		doTransformWithinSequence(information,((OAuthMediator) subject).getOutputConnector().getOutgoingLink(),sequence);
-		
-		
-	}
-	
-	private org.wso2.carbon.identity.oauth.mediator.OAuthMediator createOAuthMediator(EsbNode subject){
-		// Check subject.
-		Assert.isTrue(subject instanceof OAuthMediator, "Invalid subject."); //$NON-NLS-1$
-		OAuthMediator visualOauth = (OAuthMediator) subject;
+    public void createSynapseObject(TransformationInfo info, EObject subject, List<Endpoint> endPoints) {
+    }
 
-		// Configure properties of the mediator.
-		org.wso2.carbon.identity.oauth.mediator.OAuthMediator OauthMediator = new org.wso2.carbon.identity.oauth.mediator.OAuthMediator();
-		setCommonProperties(OauthMediator, visualOauth);
-		{
-			if (StringUtils.isNotBlank(visualOauth.getRemoteServiceUrl())) {
-				OauthMediator.setRemoteServiceUrl(visualOauth.getRemoteServiceUrl());
-			} else {
-				throw new IllegalArgumentException(
-						Messages.OAuthMediatorTransformer_Remote_Server_Url_Required_Error_Message);
-			}
-			
-			if (StringUtils.isNotBlank(visualOauth.getUsername())) {
-				OauthMediator.setUsername(visualOauth.getUsername());
-			} else {
-				throw new IllegalArgumentException(
-						Messages.OAuthMediatorTransformer_Username_Required_Error_Message);
-			}
-			
-			if (StringUtils.isNotBlank(visualOauth.getPassword())) {
-				OauthMediator.setPassword(visualOauth.getPassword());
-			} else {
-				throw new IllegalArgumentException(
-						Messages.OAuthMediatorTransformer_Password_Required_Error_Message);
-			}
-		}
-		return OauthMediator;
-	}
+    public void transformWithinSequence(TransformationInfo information, EsbNode subject, SequenceMediator sequence)
+            throws TransformerException {
+        sequence.addChild(createOAuthMediator(subject));
+        doTransformWithinSequence(information, ((OAuthMediator) subject).getOutputConnector().getOutgoingLink(),
+                sequence);
+
+    }
+
+    private org.wso2.carbon.identity.oauth.mediator.OAuthMediator createOAuthMediator(EsbNode subject) {
+
+        if (subject instanceof OAuthMediator) {
+            OAuthMediator visualOauth = (OAuthMediator) subject;
+
+            // Configure properties of the mediator.
+            org.wso2.carbon.identity.oauth.mediator.OAuthMediator OauthMediator = 
+                                                        new org.wso2.carbon.identity.oauth.mediator.OAuthMediator();
+            setCommonProperties(OauthMediator, visualOauth);
+            {
+                if (StringUtils.isNotBlank(visualOauth.getRemoteServiceUrl())) {
+                    OauthMediator.setRemoteServiceUrl(visualOauth.getRemoteServiceUrl());
+                } else {
+                    throw new IllegalArgumentException(
+                            Messages.OAuthMediatorTransformer_Remote_Server_Url_Required_Error_Message);
+                }
+
+                if (StringUtils.isNotBlank(visualOauth.getUsername())) {
+                    OauthMediator.setUsername(visualOauth.getUsername());
+                } else {
+                    throw new IllegalArgumentException(
+                            Messages.OAuthMediatorTransformer_Username_Required_Error_Message);
+                }
+
+                if (StringUtils.isNotBlank(visualOauth.getPassword())) {
+                    OauthMediator.setPassword(visualOauth.getPassword());
+                } else {
+                    throw new IllegalArgumentException(
+                            Messages.OAuthMediatorTransformer_Password_Required_Error_Message);
+                }
+            }
+            return OauthMediator;
+        } else {
+            throw new IllegalArgumentException(Messages.OAuthMediatorTransformer_InvalidSubjectErrorMessage);
+        }
+
+    }
 
 }
