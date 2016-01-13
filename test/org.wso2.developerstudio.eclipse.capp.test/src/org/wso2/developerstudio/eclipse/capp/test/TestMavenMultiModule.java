e2f5d745901b279a6894e8eee9d2522467a07ba2
knadikari <knadikari@gmail.com>
2016-01-13 11:52:38 +0530

    for testing

--- test/org.wso2.developerstudio.eclipse.capp.test/src/org/wso2/developerstudio/eclipse/capp/test/TestMavenMultiModule.java
+++ test/org.wso2.developerstudio.eclipse.capp.test/src/org/wso2/developerstudio/eclipse/capp/test/TestMavenMultiModule.java
@@ -5,10 +5,10 @@ import org.junit.runner.RunWith;
 import org.wso2.developerstudio.eclipse.swtfunctionalframework.junit.Order;
 import org.wso2.developerstudio.eclipse.swtfunctionalframework.junit.OrderedRunner;
 import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.AbstractClass;
-import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.MavenMultiModuleUtil;
+//import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.MavenMultiModuleUtil;
 import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.Setup;
 import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.constants.CompositeApllicationCons;
-import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.constants.MavenMultiModuleCons;
+//import org.wso2.developerstudio.eclipse.swtfunctionalframework.util.constants.MavenMultiModuleCons;
 
 
 @RunWith(OrderedRunner.class)
@@ -17,8 +17,8 @@ public class TestMavenMultiModule extends Setup{
 	@Test
 	@Order(order = 1)
 	public void open() throws Exception {
-		AbstractClass.openProjectFromMenu(MavenMultiModuleCons.MENU_NAME);
-		MavenMultiModuleUtil.createMavenProject("hello", "myfirst");
+		//AbstractClass.openProjectFromMenu(MavenMultiModuleCons.MENU_NAME);
+		//MavenMultiModuleUtil.createMavenProject("hello", "myfirst");
 	}
 	
 	@Test
