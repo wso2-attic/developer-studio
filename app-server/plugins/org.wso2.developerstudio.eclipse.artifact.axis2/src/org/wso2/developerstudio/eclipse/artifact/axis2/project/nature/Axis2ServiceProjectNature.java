b98681eff4a9e6faa862a39fc0ec74e6dea57e1d
awanthika <wnthksnrth@gmail.com>
2016-04-01 14:12:15 +0530

    app server upgrade to mars kernel 4.1.0

--- app-server/plugins/org.wso2.developerstudio.eclipse.artifact.axis2/src/org/wso2/developerstudio/eclipse/artifact/axis2/project/nature/Axis2ServiceProjectNature.java
+++ app-server/plugins/org.wso2.developerstudio.eclipse.artifact.axis2/src/org/wso2/developerstudio/eclipse/artifact/axis2/project/nature/Axis2ServiceProjectNature.java
@@ -25,8 +25,6 @@ import java.util.Map;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.model.PluginExecution;
-import org.apache.maven.model.Repository;
-import org.apache.maven.model.RepositoryPolicy;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.runtime.CoreException;
 import org.wso2.developerstudio.eclipse.capp.maven.utils.MavenConstants;
